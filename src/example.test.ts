4fe20b3cfea34f1074844c14208f9a12427fdf87
James Aichinger <james.aichinger@gmail.com>
2024-08-13 07:28:43 +0000

    commit repro

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,22 +1,32 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Entity, Filter, ManyToOne, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
 
 @Entity()
+@Filter({ name: 'soft-delete', default: true, cond: { deletedAt: { $eq: null } } })
 class User {
 
   @PrimaryKey()
   id!: number;
 
   @Property()
-  name: string;
+  name!: string;
 
   @Property({ unique: true })
-  email: string;
+  email!: string;
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
+  @Property({ nullable: true, type: 'timestamptz' })
+  deletedAt?: Date;
+}
 
+@Entity()
+class Submission {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  title!: string;
+
+  @ManyToOne(() => User)
+  user!: User;
 }
 
 let orm: MikroORM;
@@ -24,28 +34,47 @@ let orm: MikroORM;
 beforeAll(async () => {
   orm = await MikroORM.init({
     dbName: ':memory:',
-    entities: [User],
+    entities: [User, Submission],
     debug: ['query', 'query-params'],
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
+
+  const userA = orm.em.create(User, { name: 'User A', email: 'foo' });
+  const userB = orm.em.create(User, { name: 'User B', email: 'bar' });
+  const userC = orm.em.create(User, { name: 'User C', email: 'baz' });
+
+  orm.em.create(Submission, { title: 'Submission A', user: userA });
+  orm.em.create(Submission, { title: 'Submission B', user: userB });
+  orm.em.create(Submission, { title: 'Submission C', user: userC });
+
+  await orm.em.flush();
 });
 
 afterAll(async () => {
   await orm.close(true);
 });
 
+beforeEach(async () => {
+  orm.em.clear();
+});
+
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  const [subs, count] = await orm.em.findAndCount(Submission, {});
+
+  expect(subs).toHaveLength(3);
+  expect(count).toBe(3);
+});
+
+test('soft delete', async () => {
+  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
+  user.deletedAt = new Date();
   await orm.em.flush();
+
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const [subs, count] = await orm.em.findAndCount(Submission, {});
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  expect(subs).toHaveLength(2);
+  expect(count).toBe(2);
 });
